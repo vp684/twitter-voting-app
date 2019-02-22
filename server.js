43e208a75cdc60fd8a62a1e6b3339a8712383cc6
Viraj Patel <Viraj84@gmail.com>
2019-02-21 22:48:08 -0500

    test 3

--- server.js
+++ server.js
@@ -18,6 +18,15 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
 app.listen(port, ()=>{
 
     console.log(`server started on port: ${port}`)
