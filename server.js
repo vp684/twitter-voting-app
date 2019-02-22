de07b71d72fe7867ab26ca5a8bf1db991f7e7146
Viraj Patel <Viraj84@gmail.com>
2019-02-22 18:24:50 -0500

    changes

--- server.js
+++ server.js
@@ -16,35 +16,38 @@ const app = express();
 let dberr = null
 
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
+
 
    //connect to db on err report to user no functionality
-   mongo.connect(process.env.MLAB_URI, {useNewUrlParser: true}, (err, db)=>{
+mongo.connect(process.env.MLAB_URI, {useNewUrlParser: true}, (err, db)=>{
     if(err){
         //handle db error.  front end shouldnt be able to login etc...
         console.log('Database error: ' + err)
-        app.get('*', (req, res)=>{
-            console.log(err)
-            res.send({data: "db error"})               
-        })
+        // app.get('*', (req, res)=>{
+        //     console.log(err)
+        //     res.send({data: "db error"})               
+        // })
     }else{
         // connected to db...
         //get db
         let database = db.db('fcc')
-
+        console.log(database)
         //routes
         app.get('/api/data', (req, res)=>{
             res.send({data: `this is from the API  `})               
-        })
+        })    
+        
         
 
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+        // Serve any static files
+        app.use(express.static(path.join(__dirname, 'client/build')));
+        // Handle React routing, return all requests to React app
+        // app.get('*', function(req, res) {
+        //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        // });
     }
 
 })
